8fc5acca85e1fc7088a1d840849ef4d64f7fa6b9
laoxu93 <110043107+laoxu93@users.noreply.github.com>
2026-08-02 10:23:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# GitHub: https://github.com/laoxu93/led
 #
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# -------------------------------------------------------------------
+# 1. 修改默认 LAN 口 IP 地址 (默认 192.168.1.1 -> 192.168.2.1)
+# -------------------------------------------------------------------
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# -------------------------------------------------------------------
+# 2. 修改默认主机名
+# -------------------------------------------------------------------
+sed -i 's/OpenWrt/N60PRO-Router/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# -------------------------------------------------------------------
+# 3. 预装最新版 Argon 主题并替换老版本
+# -------------------------------------------------------------------
+# 删除源码中可能存在的旧版 argon
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf package/downloads/luci-theme-argon
+
+# 克隆最新版 argon 主题及配置插件
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/downloads/luci-app-argon-config
+
+# 将默认主题修改为 Argon
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# -------------------------------------------------------------------
+# 4. 修复/优化系统默认参数 (如：设置默认时区为上海)
+# -------------------------------------------------------------------
+sed -i "s/'UTC'/'CST-8'/g" package/base-files/files/bin/config_generate
+sed -i "/timezone='CST-8'/a \ \ \ \ \ \ \ \ set system.@system[0].zonename='Asia/Shanghai'" package/base-files/files/bin/config_generate
