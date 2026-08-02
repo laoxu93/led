5c8466f1515cfbc20dc9e9658974eb2d12b1d260
laoxu93 <110043107+laoxu93@users.noreply.github.com>
2026-08-02 10:23:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# GitHub: https://github.com/laoxu93/led
 #
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# -------------------------------------------------------------------
+# 1. 引入 PassWall 及其完整的核心依赖包仓库 (解决 dependency 缺失报错)
+# -------------------------------------------------------------------
+echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' >> feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
 
-# Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# -------------------------------------------------------------------
+# 2. (可选) 引入 OpenClash 源码
+# -------------------------------------------------------------------
+# echo 'src-git openclash https://github.com/vernesong/OpenClash.git;master' >> feeds.conf.default
+
+# -------------------------------------------------------------------
+# 3. (可选) 取消默认 feeds 注释或替换镜像
+# -------------------------------------------------------------------
+# sed -i 's/^#\(.*telephony\)/\1/' feeds.conf.default
